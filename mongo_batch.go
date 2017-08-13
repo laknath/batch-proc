fe2c7f5fba4c326033a3d13675890917e306080c
Laknath <blaknath@gmail.com>
2017-08-13 17:57:40 +0530

    Fix FetchInput to return results

--- mongo_batch.go
+++ mongo_batch.go
@@ -1,23 +1,31 @@
 package mongobatch
 
 import (
-	"github.com/laknath/go-mongo-batch/config"
 	"gopkg.in/mgo.v2"
 )
 
-func FetchInput(conf *config.Configuration, result interface{}) (interface{}, error) {
+// FetchInput fetches defined lengths of batches from a Mongo collection.
+// The retrieved batch will be marked processing.If a record is not marked
+// as "processed" within a given time interval,the record will be reverted
+// back to the initial non-processed state.This workflow supports concurrent processing.
+
+// The conf argument is a Configuration object initialized with values necessary for
+// the mongo connection.
+
+// The result argument must be the address for a slice. It will hold the resulting result set.
+func FetchInput(conf *Configuration, result interface{}) error {
 	//TODO
-	//use a distributed lock for atomicity
-	session, err := mgo.Dial(config.ConnectString(conf))
+	//use a distributed lock for mutual exclusion
+	session, err := mgo.Dial(connectString(conf))
 	if err != nil {
-		return nil, err
+		return err
 	}
 	defer session.Close()
 	// query db
 	c := session.DB(conf.Database).C(conf.Collection)
 	iter := c.Find(conf.FetchQuery).Limit(conf.FetchLimit).Sort(conf.FetchOrder).Iter()
 
-	err = iter.All(&result)
+	err = iter.All(result)
 
-	return result, err
+	return err
 }
