4710a432e829ca21c132ebb95636a0bae17bcb9d
Laknath <blaknath@gmail.com>
2017-08-16 17:22:00 +0530

    Mark fetched records as Processing

--- mongo_batch.go
+++ mongo_batch.go
@@ -2,12 +2,13 @@ package mongobatch
 
 import (
 	"gopkg.in/mgo.v2"
+	"reflect"
 )
 
 // FetchInput fetches defined lengths of batches from a Mongo collection.
-// The retrieved batch will be marked processing.If a record is not marked
-// as "processed" within a given time interval,the record will be reverted
-// back to the initial non-processed state.This workflow supports concurrent processing.
+// The retrieved batch will be marked processing. If a record is not marked
+// as "processed" within a given time interval, the record will be reverted
+// back to the initial non-processed state. This workflow supports concurrent processing.
 
 // The conf argument is a Configuration object initialized with values necessary for
 // the mongo connection.
@@ -35,7 +36,39 @@ func FetchInput(conf *Configuration, result interface{}) error {
 	c := session.DB(conf.Database).C(conf.Collection)
 	iter := c.Find(conf.FetchQuery).Limit(conf.FetchLimit).Sort(conf.FetchOrder).Iter()
 
-	err = iter.All(result)
+	err = markProcessing(conf, iter, result)
 
 	return err
+}
+
+// markProcessing updates the state of records in the iter to "processing"
+// and stores records in the result slice.
+func markProcessing(conf *Configuration, iter *mgo.Iter, result interface{}) error {
+	resultv := reflect.ValueOf(result)
+	if resultv.Kind() != reflect.Ptr || resultv.Elem().Kind() != reflect.Slice {
+		panic("result argument must be a slice address")
+	}
+	slicev := resultv.Elem()
+	slicev = slicev.Slice(0, slicev.Cap())
+	elemt := slicev.Type().Elem()
+	i := 0
+	for {
+		if slicev.Len() == i {
+			elemp := reflect.New(elemt)
+			if !iter.Next(elemp.Interface()) {
+				break
+			}
+			slicev = reflect.Append(slicev, elemp.Elem())
+			slicev = slicev.Slice(0, slicev.Cap())
+		} else {
+			if !iter.Next(slicev.Index(i).Addr().Interface()) {
+				break
+			}
+		}
+
+		slicev.Index(i).FieldByName(conf.StateFld).SetString("processing")
+		i++
+	}
+	resultv.Elem().Set(slicev.Slice(0, i))
+	return iter.Close()
 }
