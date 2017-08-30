5cda9138141f14a9f455df4dba3be4078972e5be
Laknath <blaknath@gmail.com>
2017-08-30 22:59:07 +0530

    Update records to processed state
    Support for following 3 strategies.
    * Time based updates
    * Minimum number of records based update
    * Instant updates

--- mongo_update.go
+++ mongo_update.go
@@ -1,9 +1,11 @@
 package mongobatch
 
 import (
+	"encoding/hex"
 	"github.com/laknath/go-mongo-batch/internal"
 	"gopkg.in/mgo.v2"
 	"gopkg.in/mgo.v2/bson"
+	"log"
 	"reflect"
 	"time"
 )
@@ -17,7 +19,6 @@ func UpdateBatch(conf *Configuration, result interface{}) (chan interface{}, err
 	if err != nil {
 		return nil, err
 	}
-	defer session.Close()
 	// query db
 	c := session.DB(conf.Database).C(conf.Collection)
 
@@ -30,12 +31,29 @@ func UpdateBatch(conf *Configuration, result interface{}) (chan interface{}, err
 	}
 
 	go func() {
+		defer session.Close()
+		minr := int(conf.UpdateStrategy.MinRecords)
+
 		for {
 			select {
 			case v := <-ch: // receiving stream
-				reflect.Append(s, reflect.ValueOf(v))
+				if !conf.UpdateStrategy.UseMinRecords {
+					//update as they arrive
+					go updateSingleRecord(conf, v, c)
+				} else {
+					s = reflect.Append(s, reflect.ValueOf(v))
+					log.Println(s.Len())
+
+					if s.Len() >= minr {
+						log.Println("Updating the batch")
+						s = updateAndClear(conf, s, c)
+					}
+				}
+
 			case _ = <-t: // the max interval to go without updating records. nil channel if disabled.
-				updateRecords(conf, s.Addr(), c)
+				if s.Len() > 0 {
+					s = updateAndClear(conf, s, c)
+				}
 			}
 		}
 	}()
@@ -43,9 +61,37 @@ func UpdateBatch(conf *Configuration, result interface{}) (chan interface{}, err
 	return ch, nil
 }
 
-func updateRecords(conf *Configuration, results interface{}, c *mgo.Collection) error {
-	ids := fetchIds(conf, results)
+// updateAndClear updates mongo records and empties the slice if update was successful.
+func updateAndClear(conf *Configuration, slicev reflect.Value, c *mgo.Collection) reflect.Value {
+	if err := updateRecords(conf, slicev, c); err != nil {
+		log.Printf("Not updated. %v", err)
+		return slicev
+	}
+
+	return reflect.MakeSlice(slicev.Type(), 0, 0)
+}
+
+// updateRecords updates mongo records to processed state.
+func updateRecords(conf *Configuration, slicev reflect.Value, c *mgo.Collection) error {
+	ids := fetchIds(conf, slicev)
 	// update all matching documents to processing
 	_, err := c.UpdateAll(bson.M{"_id": bson.M{"$in": ids}}, bson.M{"$set": bson.M{conf.StateFld: conf.ProcessedState}})
 	return err
+}
+
+// updateSingleRecord verifies the validity of v and updates the state of the document
+// to processed in mongo.
+func updateSingleRecord(conf *Configuration, v interface{}, c *mgo.Collection) {
+	resultv := reflect.ValueOf(v)
+	if resultv.Kind() != reflect.Ptr || resultv.Elem().Kind() != reflect.Struct {
+		log.Printf("Not updated. Record is not a pointer to a struct %v", v)
+		return
+	}
+	id := resultv.Elem().FieldByName("Id")
+	if id.IsValid() {
+		oid := bson.ObjectIdHex(hex.EncodeToString([]byte(id.String())))
+		c.UpdateId(oid, bson.M{"$set": bson.M{conf.StateFld: conf.ProcessedState}})
+	} else {
+		log.Printf("Not updated. Struct doesn't have an ID field %v", v)
+	}
 }
